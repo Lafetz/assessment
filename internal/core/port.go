11a6d40c83e5f425d407ed80d2ed3de07b6c2e20
lafetz <yafetyohaness@gmail.com>
2024-10-27 16:12:20 +0300

    impl core logic

--- internal/core/port.go
+++ internal/core/port.go
@@ -1,1 +1,15 @@
 package person
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type repository interface {
+	AddPerson(ctx context.Context, id uuid.UUID) error
+	GetPerson(ctx context.Context, id uuid.UUID) (Person, error)
+	GetPersons(ctx context.Context) ([]Person, error)
+	DeletePerson(ctx context.Context, id uuid.UUID) error
+	UpdatePerson(ctx context.Context, person Person) error
+}
